dd287615276e0c816027a404ba4c6f9ff29341c9
Xabi <xabier.martinez@arex.io>
2022-10-20 08:01:30 +0200

    update links handler

--- src/components/links/section-wrap.vue
+++ src/components/links/section-wrap.vue
@@ -6,8 +6,8 @@ import socialButton from './social-button.vue';
 <div class="flex w-full justify-center max-w-md border-white mx-auto">
     <social-button link="https://twitter.com/xabi_03" :icon="['fab','twitter']" />
     <social-button link="https://www.linkedin.com/in/xmartinez" :icon="['fab','linkedin']" />
-    <social-button link="https://github.com/xabi93" :icon="['fab','github']" />
-    <social-button link="https://www.instagram.com/xabi_93" :icon="['fab','instagram']" />
+    <social-button link="https://github.com/x4b1" :icon="['fab','github']" />
+    <social-button link="https://www.instagram.com/x4b1" :icon="['fab','instagram']" />
     <social-button link="mailto:hello@xmartinez.dev" :icon="['far','paper-plane']" />
 </div>
 </template>
